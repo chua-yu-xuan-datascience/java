69a4fffdf06455be80eefac104445e51ac935415
Chua Yu Xuan <cyx2811@gmail.com>
2025-01-20 10:17:01 +0800

    abracadabra

--- Abracadabra.java
+++ Abracadabra.java
@@ -4,11 +4,12 @@ public class Abracadabra {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        //int N = scanner.nextInt();
         
-        // Loop through 1 to N and print the output
-        for (int i = 1; i <= 10; i++) {
+        //input
+        int N = scanner.nextInt();
+        
+        // Iterate from 1 to N
+        for (int i = 1; i <= N; i++) {
             System.out.println(i + " Abracadabra");
         }
         
