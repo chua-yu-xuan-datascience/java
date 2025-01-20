8be00074cb90f8e6f1f6abd9327bc5e3c1233458
Chua Yu Xuan <cyx2811@gmail.com>
2025-01-20 10:42:23 +0800

    commit2

--- OnesFactorial.java
+++ OnesFactorial.java
@@ -2,22 +2,21 @@ import java.util.Scanner;
 
 public class OnesFactorial {
     public static void main(String[] args) {
-        // Hardcoded last digits of factorials from 0! to 10!
+        // last digits
         int[] onesdigit = {1, 2, 6, 4, 0, 0, 0, 0, 0, 0};
 
-        // Create a scanner to read input
         Scanner scanner = new Scanner(System.in);
 
-        // Read the number of test cases
-        int T = scanner.nextInt();
+        // first input is number of iterations
+        //int T = scanner.nextInt();
 
-        // Process each test case
-        for (int t = 1; t < T; t++) {
-            int N = scanner.nextInt();
+        // iterate through the next inputs
+        for (int t = 1; t <= 3; t++) {
+            //int N = scanner.nextInt();
+            int[] N = {1, 6, 2};
             System.out.println(onesdigit[N-1]);
         }
 
-        // Close the scanner
         scanner.close();
     }
 }
